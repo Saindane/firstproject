ea3f36bf205ffdcc59298512af291d569a9171ad
Lalitkumar <lalitkumar@Lalitkumar.local>
2018-09-11 10:53:40 +0530

    express app

--- nodejs/10-sept/async.js
+++ nodejs/10-sept/async.js
@@ -12,7 +12,7 @@ var users = [{"id":1,"email":"abc@gmail.com","name":"Alex","age":22},
 
 
 
-//url = http://localhost:8087/allusers
+//url = http://localhost:8088/allusers
 app.get('/allusers', function (req, res,/* next*/) {
     async.series([
         function(callback) {
@@ -31,9 +31,8 @@ app.get('/users/:id?', function (req, res,next) {
     var flag = false;
 
     async.series([
-
-     function(callback){
-
+        //This function checking id is present or not
+     function checking(callback){
             users.forEach(function (obj) {
                 if(obj.id === reqparam){
     
@@ -54,17 +53,19 @@ app.get('/users/:id?', function (req, res,next) {
             callback();
         }
 },
-     function(callback){
-         callback(null,user);
+    //This function display data
+     function display(callback){
+         callback(user);
        }
-    ],function(error,Data){
+    ],function(error,data){
        if(error){
         res.send(error);
       }else{
-        res.send(Data);
+        res.send(data);
       }
     }) 
 });
+
 
 //url = http://localhost:8088/users/7
 app.post('/users/:id?', function (req, res,next) {
@@ -79,9 +80,8 @@ app.post('/users/:id?', function (req, res,next) {
       }
 
     async.series([
-
-     function(callback){
-
+     //This function checking id is present or not
+     function checking(callback){
         users.forEach(function (obj) {
             console.log(typeof obj.id);
             if(obj.id === reqparam){
@@ -90,26 +90,118 @@ app.post('/users/:id?', function (req, res,next) {
          });
 
         if(flag === true) {
-             callback('User is already added');
-           }
-        else{
+             callback('Data already exist');
+           }else{
            callback();
         }
 },
-    function(callback){
+   //This function posting the data into array
+    function display(callback){
      users.push(newUser);
-     callback(null,users)
+     callback(users)
   }
 
-    ],function(error,done){
+],function(error,data){
        if(error){
         res.send(error);
       }
       else{       
-        res.send(done);
+        res.send(data);
     }
     }) 
 });
+
+
+//url = http://localhost:8088/user/4
+app.put('/user/:id', function (req, res,next) {
+    var flag = false;
+     var id = parseInt(req.params.id);
+
+    async.series([
+     //This function checking id is present or not
+     function checking(callback){
+        users.forEach(function (obj) {
+            if(obj.id === id){
+             flag = true;
+            }
+         });
+
+          if(flag === false)
+          {
+             callback(" No data found to upadate");
+          }else{
+            callback();
+         }
+  
+},
+    //This function updating data 
+    function display(callback){
+        users.forEach(function (obj) {
+
+            if(obj.id === id){
+              obj.email=req.body.email;
+              obj.name= req.body.name;
+              obj.age=req.body.age;
+              callback(users)
+            }
+         });
+  }
+],function(error,data){
+       if(error){
+        res.send(error);
+      }
+      else{       
+        res.send(data);
+    }
+    }) 
+});
+
+
+//url = http://localhost:8088/user/4
+app.delete('/user/:id', function (req, res,next) {
+    var flag = false;
+     var id = parseInt(req.params.id);
+
+    async.series([
+     //This function checking id is present or not
+     function checking(callback){
+        users.forEach(function (obj) {
+            if(obj.id === id){
+             flag = true;
+            }
+         });
+
+          if(flag === false)
+          {
+             callback("No data found");
+          }else{
+            callback();
+         }
+  
+},
+   //This function deleteing data
+    function display(callback){
+        users.forEach(function (obj) {
+
+            if(obj.id === id){
+                var index = users.indexOf(obj);
+                users.splice(index, 1);
+                callback(users)
+            }
+         });
+  }
+],function(error,data){
+       if(error){
+        res.send(error);
+      }
+      else{       
+        res.send(data);
+    }
+    }) 
+});
+
+
+
 
 
 var server = app.listen(8088, function () {
