caaa1578f269193a3bb7e40cb4471b831800056e
Lalitkumar <lalitkumar@Lalitkumar.local>
2018-09-14 10:51:37 +0530

    express app

--- nodejs/10-sept/asyncmongo.js
+++ nodejs/10-sept/asyncmongo.js
@@ -28,20 +28,19 @@ app.get('/allusers/:id', function(req, res) {
 
     async.series([
         //This function checking id is present or not
-        /*  function(callback) {
-              User.findOne({ "id": id }, function(err, u) {
-                  console.log(u);
-                  console.log(typeof u);
-                  if (u == null) {
-                      flag = true;
-                  }
-              })
-              if (flag === true) {
-                  callback('No data found');
-              } else {
-                  callback();
-              }
-          },*/
+        function(callback) {
+            User.find({ "id": id }, function(err, docs) {
+                if (docs.length) {
+                    flag = true;
+                }
+                if (flag === false) {
+                    callback('No Data Found');
+                } else {
+                    callback();
+                }
+            })
+        },
+        //This function giving single user
         function(callback) {
             User.find({ "id": id }, function(err, data) {
                 if (err) {
@@ -57,7 +56,7 @@ app.get('/allusers/:id', function(req, res) {
         if (error) {
             res.send(error);
         } else {
-            res.send(data);
+            res.send(data[1]);
         }
     })
 
@@ -67,32 +66,31 @@ app.get('/allusers/:id', function(req, res) {
 //This is for posting the single data
 //url =  http://localhost:8090/adduser/root@gmail.com
 app.post('/adduser/:email', function(req, res) {
-    let id = parseInt(req.params.id);
+    let email = req.params.email;
     flag = false;
 
     let user = new User();
-    user.id = id;
+    user.id = req.body.id;
     user.name = req.body.name;
-    user.email = req.body.email;
+    user.email = email;
     user.age = req.body.age;
 
 
     async.series([
-        //This function checking id is present or not
-        /*  function(callback) {
-              User.findOne({ "id": id }, function(err, u) {
-                  console.log(u);
-                  console.log(typeof u);
-                  if (u == null) {
-                      flag = true;
-                  }
-              })
-              if (flag === true) {
-                  callback('No data found');
-              } else {
-                  callback();
-              }
-          },*/
+        //This function checking email is present or not
+        function(callback) {
+            User.find({ "email": email }, function(err, docs) {
+                if (docs.length) {
+                    flag = true;
+                }
+                if (flag === true) {
+                    callback('Data is already exist');
+                } else {
+                    callback();
+                }
+            })
+        },
+        //This function is storing data into database
         function(callback) {
             user.save(function(err) {
                 if (err) {
@@ -100,7 +98,6 @@ app.post('/adduser/:email', function(req, res) {
                     return;
                 } else {
                     User.find({}, function(err, users) {
-                        console.log(users);
                         callback(null, users);
                     })
                 }
@@ -110,7 +107,7 @@ app.post('/adduser/:email', function(req, res) {
         if (error) {
             res.send(error);
         } else {
-            res.send(data);
+            res.send(data[1]);
         }
     })
 
@@ -128,22 +125,20 @@ app.put('/user/:id', function(req, res) {
 
     async.series([
         //This function checking id is present or not
-        /* function(callback) {
-             User.findOne({ "id": id }, function(err, u) {
-                 console.log(u);
-                 console.log(typeof u);
-                 if (u == null) {
-                     flag = true;
-                 }
-             })
-             if (flag === true) {
-                 callback('No data found');
-             } else {
-                 callback();
-             }
-         },*/
         function(callback) {
-
+            User.find({ "id": id }, function(err, docs) {
+                if (docs.length) {
+                    flag = true;
+                }
+                if (flag === false) {
+                    callback('No Data Found to Update');
+                } else {
+                    callback();
+                }
+            })
+        },
+        //This function is for updateing data from database
+        function(callback) {
             User.updateOne({ "id": id }, user, function(err) {
                 if (err) {
                     console.log(err);
@@ -160,7 +155,7 @@ app.put('/user/:id', function(req, res) {
         if (error) {
             res.send(error);
         } else {
-            res.send(data);
+            res.send(data[1]);
         }
     })
 })
@@ -168,24 +163,24 @@ app.put('/user/:id', function(req, res) {
 //Delete operation is here
 //url =  http://localhost:8090/user/1
 app.delete('/user/:id', function(req, res) {
+    flag = false;
     let id = parseInt(req.params.id);
 
     async.series([
         //This function checking id is present or not
-        /* function(callback) {
-             User.findOne({ "id": id }, function(err, u) {
-                 console.log(u);
-                 console.log(typeof u);
-                 if (u == null) {
-                     flag = true;
-                 }
-             })
-             if (flag === true) {
-                 callback('No data found');
-             } else {
-                 callback();
-             }
-         },*/
+        function(callback) {
+            User.find({ "id": id }, function(err, docs) {
+                if (docs.length) {
+                    flag = true;
+                }
+                if (flag === false) {
+                    callback('No Data Found to Delete');
+                } else {
+                    callback();
+                }
+            })
+        },
+        //This function is for removeing data from database
         function(callback) {
             User.remove({ "id": id }, function(err) {
                 if (err) {
@@ -203,25 +198,10 @@ app.delete('/user/:id', function(req, res) {
         if (error) {
             res.send(error);
         } else {
-            res.send(data);
+            res.send(data[1]);
         }
     })
-
-
-
-
 })
-
-
-
-
-
-
-
-
-
-
-
 
 var server = app.listen(8090, function() {
 
